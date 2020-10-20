481e67ff83c64e42304c62842734b9f45e66269c
Matheus Henrique Coelho dos Anjos <matheushcoelho1@gmail.com>
2020-10-20 00:24:59 -0300

    Commit all program

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,11 @@
+import React, { Children } from 'react';
+
+function ButtonLink(props) {
+    return (
+        <a href={props.href} className={props.className}>
+            {props.children}
+        </a>
+    );
+} 
+
+export default ButtonLink;
